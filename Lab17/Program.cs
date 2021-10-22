6cb12beed2e53b43eedf49790056c0ada17b86bf
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2021-10-22 09:23:40 +0500

    Добавьте файлы проекта.

---
+++ Lab17/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check<int> check1 = new Check<int>();
+            check1.Number = 100;
+            check1.Name = "Иванов Иван Иванович";
+            check1.Balance = 10000;
+            check1.Output();
+            Console.WriteLine();
+
+            Check<string> check2 = new Check<string>();
+            check2.Number = "GR-1100";
+            check2.Name = "Сидоров Сергей Петрович";
+            check2.Balance = 12345;
+            check2.Output();
+            Console.WriteLine();
+
+            Check<float> check3 = new Check<float>();
+            check3.Number = 100.54f;
+            check3.Name = "Николаев Петр Викторович";
+            check3.Balance = 12345;
+            check3.Output();
+            Console.WriteLine();
+
+            Check<int> check4 = new Check<int>();
+            check4.Number = 55241;
+            check4.InputFioBalance();
+            check4.Output();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        public class Check<T>
+        {
+            public T Number { get; set; }
+            public double Balance { get; set; }
+            public string Name { get; set; }
+
+
+            //public void Input<T>()                                       // метод ввода не работает, можно ли заполнять с клавиатуры переменную Number (номер счета)?
+            //{
+            //    Console.WriteLine("Введите номер счета");
+            //    value = Console.ReadLine();
+            //    Console.WriteLine("Введите Ф.И.О. владельца");
+            //    Name = Console.ReadLine();
+            //    Console.WriteLine("Введите баланс");
+            //    Balance = Convert.ToDouble(Console.ReadLine());
+            //}
+
+
+            public void InputFioBalance()
+            {
+                Console.WriteLine("Введите Ф.И.О. владельца");
+                Name = Console.ReadLine();
+                Console.WriteLine("Введите баланс");
+                Balance = Convert.ToDouble(Console.ReadLine());
+            }
+
+            public void Output()
+            {
+                Console.WriteLine($"Владелец счета: {Name}, Счет {Number}, Баланс счета {Balance,3:n}");
+            }
+
+        }
+    }
+}
+
